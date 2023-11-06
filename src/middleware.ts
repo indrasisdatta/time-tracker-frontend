fbebfcd68eb0ade86bffd12f71d1d26ec2446b83
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-11-06 10:42:15 +0530

    Middleware route authentication, cookie var set in env

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  // console.log("Request: ", request);
-  return NextResponse.redirect(new URL("/auth/login", request.url));
+export async function middleware(request: NextRequest) {
+  // console.log("Request cookie -->> ", request.cookies.get(process.env.NEXT_PUBLIC_USER_LS_KEY));
+  if (request.cookies.has(process.env.NEXT_PUBLIC_USER_LS_KEY!)) {
+    let tokenObj = request.cookies.get(
+      process.env.NEXT_PUBLIC_USER_LS_KEY!
+    )?.value;
+    tokenObj = JSON.parse(tokenObj as string);
+    console.log("User token: ", tokenObj);
+  } else {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 }
 
 export const config = {
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,44 +4,27 @@ import { isServer } from "@/utils/helper";
 import { cookies } from "next/headers";
 
 /* Local storage key to store logged in user data */
-const USER_LS_KEY = "userData";
-
-// export const LoggedinUserData = {
-//   set: (userObj: LoggedinUser) => {
-//     if (!isServer() && !!userObj) {
-//       return localStorage.setItem(USER_LS_KEY, JSON.stringify(userObj));
-//     }
-//   },
-//   get: () => {
-//     if (!isServer() && !!localStorage.getItem(USER_LS_KEY)) {
-//       return JSON.parse(localStorage.getItem(USER_LS_KEY) as string);
-//     }
-//     return null;
-//   },
-//   clear: () => {
-//     if (!isServer()) {
-//       localStorage.removeItem(USER_LS_KEY);
-//     }
-//   },
-// };
 
 export const setLoggedinUserData = async (userObj: LoggedinUser) => {
-  return cookies().set(USER_LS_KEY, JSON.stringify(userObj));
+  return cookies().set(
+    process.env.NEXT_PUBLIC_USER_LS_KEY!,
+    JSON.stringify(userObj)
+  );
 };
 
 export const getLoggedinUserData = async () => {
-  console.log(`Reading cookie ${USER_LS_KEY}`, cookies().get(USER_LS_KEY));
-  if (cookies().has(USER_LS_KEY)) {
-    return JSON.parse(cookies().get(USER_LS_KEY)!.value);
+  // console.log(`Reading cookie ${USER_LS_KEY}`, cookies().get(USER_LS_KEY));
+  if (cookies().has(process.env.NEXT_PUBLIC_USER_LS_KEY!)) {
+    try {
+      return JSON.parse(
+        cookies().get(process.env.NEXT_PUBLIC_USER_LS_KEY!)!.value
+      );
+    } catch (e) {
+      return null;
+    }
   }
   return null;
 };
 export const deleteLoggedinUserData = async () => {
-  // return cookies().delete(USER_LS_KEY);
-  return cookies().set({
-    name: USER_LS_KEY,
-    value: "",
-    expires: new Date("2016-10-05"),
-    path: "/", // For all paths
-  });
+  return cookies().delete(process.env.NEXT_PUBLIC_USER_LS_KEY!);
 };
