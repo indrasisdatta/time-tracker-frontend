ab0f89b813a17c79d37552de9eec625c2aee9f92
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-11-20 10:49:11 +0530

    Axios interceptor to inject access token in Authorization header

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,20 @@
+import { getLoggedinUserData } from "@/utils/auth";
 import Axios from "axios";
 
-export const axios = Axios.create({
+const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
+
+/* Inject access token in Authorization header */
+axios.interceptors.request.use(async (config) => {
+  const loggedinData = await getLoggedinUserData();
+  if (
+    typeof loggedinData === "object" &&
+    loggedinData.hasOwnProperty("accessToken")
+  ) {
+    config.headers.Authorization = `Bearer ${loggedinData.accessToken}`;
+  }
+  return config;
+});
+
+export { axios };
